fd5ac37a2ce1451c8f7c12d362b199a1fcf5392f
zhw <907413053@qq.com>
2018-12-06 16:19:17 +0800

    用户session_key添加

--- app/Api/Controllers/AuthController.php
+++ app/Api/Controllers/AuthController.php
@@ -32,11 +32,7 @@ class AuthController extends Controller
             // 下面为可选项
             // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
             'response_type' => 'array',
-
-            'log' => [
-                'level' => 'debug',
-                'file' => __DIR__.'/wechat.log',
-            ],
+            
         ];
         $code = request('code');
         $iv = request('iv');
